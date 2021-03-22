956e6e0c3c2fa2e92be8aac34c9ed729109be0df
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-22 15:35:57 +0100

    initialize class Dice and method throwDice()

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,24 @@
+import java.util.Random;
 
 public class Dice {
+	private int value;
+	
+	public Dice(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public void throwDice() {
+		Random rd = new Random();
+		this.value = rd.nextInt(6)+1;
+		System.out.println("Get the value : "+getValue());
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Dice dice = new Dice(0);
+		dice.throwDice();
+		
 
 	}
 
