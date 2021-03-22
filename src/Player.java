84bbbf17653fff949962f09948706e59d09e7054
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-22 15:53:58 +0100

    initialize class Player

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,29 @@
 
 public class Player {
+	private String name;
+	private int totalPoints;
+	
+	public Player(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTotalPoints() {
+		return totalPoints;
+	}
+
+	public void setTotalPoints(int totalPoints) {
+		this.totalPoints = totalPoints;
+	}
+	
+	
+	
 
 }
